878c199d07801da068971892cba42b590ce8e55d
j-mattias <mattiasjohansson.vfx@gmail.com>
2024-07-05 11:09:41 +0200

    Switched to correct word

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,6 +1,6 @@
 for (let num = 1; num <= 100; num++) {
     if ((num % 3 === 0) && (num % 5 === 0)) {
-        console.log(`FizzBuzz (${num} / 3 or 5)`);
+        console.log(`FizzBuzz (${num} / 3 and 5)`);
     } else if (num % 3 === 0) {
         console.log(`Fizz (${num} / 3)`);
     } else if (num % 5 === 0) {

ac722c7ec1668a5a7585056909a41955bf7d2ec0
j-mattias <mattiasjohansson.vfx@gmail.com>
2024-07-05 10:55:34 +0200

    Swapped condition and added declaration that I forgot.

--- going_to_boston/going_to_boston.js
+++ going_to_boston/going_to_boston.js
@@ -4,8 +4,8 @@ let totalScore = 0, diceThrowsLeft = 3;
 for (; diceThrowsLeft > 0; diceThrowsLeft--) {
     let highestRoll = 0;
 
-    for (let i = 0; diceThrowsLeft > i; i++) {
-        dice = Math.floor(Math.random() * 6 + 1);
+    for (let i = 0; i < diceThrowsLeft; i++) {
+        let dice = Math.floor(Math.random() * 6 + 1);
         highestRoll = (dice > highestRoll) ? dice : highestRoll;
         console.log(dice);
     }
